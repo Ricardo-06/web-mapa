95edeb445ad87ebab960af3941415224bb84ac7b
Ricardo-06 <ricardofonseca@ufba.br>
2025-01-09 18:34:13 -0300

    Add files via upload

---
+++ styles/ne_10m_airportsne_10m_airportsshp_1_style.js
@@ -0,0 +1,42 @@
+var size = 0;
+var placement = 'point';
+
+var style_ne_10m_airportsne_10m_airportsshp_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    var clusteredFeatures = feature.get("features");
+    var labelFont = "18.2px \'Arial\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    size = clusteredFeatures.length;
+    var textAlign = "center";
+    var offsetX = 0;
+    var offsetY = 0;
+    if (size == 1) {
+        textAlign = "left"
+        offsetX = 8
+        offsetY = 3
+        var feature = clusteredFeatures[0];
+        if (feature.get("iata_code") !== null && resolution > 0 && resolution < 2800) {
+            labelText = String(feature.get("iata_code"));
+        }
+        key = value + "_" + labelText
+    } else {
+        labelText = size.toString()
+        size = 2*(Math.log(size)/ Math.log(2))
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(229,182,54,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
